25c2dee7649205166d7ba43aca816a6a5249a076
skps2010 <skps2010030908@yahoo.com>
2023-07-19 16:48:48 +0800

    make basic search api

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from fastapi import FastAPI
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openai.api_key = os.getenv('CHATGPT_TOKEN')
+
+
+def get_description(place: str):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"請給予「{place}」這個地區的簡介，在一百字以內",
+        max_tokens=1024,
+        temperature=0.5,
+    )
+    # print(response)
+
+    return response["choices"][0]["text"]
 
 
 def create_app():
@@ -7,15 +25,11 @@ def create_app():
     @app.get("/")
     def index():
         # TODO: maybe return some information about this service?
-        return ""
+        return "hi"
 
-    @app.get("/healthz")
-    def health_check():
-        return "ok"
-
-    @app.get("/readyz")
-    def readiness_check():
-        return "ok"
+    @app.get("/search")
+    def search(place: str = '六堆'):
+        return get_description(place)
 
     @app.on_event("startup")
     def on_startup():
