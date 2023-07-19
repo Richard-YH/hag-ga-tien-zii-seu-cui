41a2a7f307b1d026084828b6cc6f48c3e8affd0a
skps2010 <skps2010030908@yahoo.com>
2023-07-19 20:56:45 +0800

    add sound button

--- main.py
+++ main.py
@@ -3,21 +3,40 @@ from fastapi.responses import HTMLResponse
 import openai
 from dotenv import load_dotenv
 import os
+import json
+import requests
 
 load_dotenv()
 openai.api_key = os.getenv('CHATGPT_TOKEN')
+baseurl = 'http://203.145.221.230:10101'
+mandarin_to_hakka_url = baseurl + '/run/predict'
 
 
 def get_description(place: str):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f"請給予「{place}」這個地區的簡介，在一百字以內",
+        prompt=f"請給予「{place}」這個地區的簡介，在五十字以內",
         max_tokens=1024,
         temperature=0.5,
     )
-    # print(response)
 
-    return response["choices"][0]["text"]
+    text = response["choices"][0]["text"]
+    audio_json = json.dumps({"fn_index": 0, "data": [text]})
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(mandarin_to_hakka_url,
+                             data=audio_json,
+                             headers=headers)
+    if response.status_code == 200:
+        json_response = json.loads(response.text)
+        audiopath = json_response['data'][0]['name']
+        download_link = f'{baseurl}/file={audiopath}'
+    else:
+        download_link = 'error'
+
+    return {
+        'text': text,
+        'sound_link': download_link,
+    }
 
 
 def create_app():
